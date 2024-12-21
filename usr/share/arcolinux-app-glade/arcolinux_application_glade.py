a2fa34152bbce3d233f53aeffbbcf7ae6cd981a1
arcolinuxz <arcolinuxinfo@gmail.com>
2024-12-21 07:49:01 +0100

    arcoinstall iso

--- usr/share/arcolinux-app-glade/arcolinux_application_glade.py
+++ usr/share/arcolinux-app-glade/arcolinux_application_glade.py
@@ -705,6 +705,88 @@ class Main:
         fn.permissions(destination)
         logging.info("Check your home directory for the iso")
 
+    def on_create_arcoinstall_clicked(self, widget):
+        # Creation of the arcoinstall iso
+        logging.info("Arcoinstall iso selected")
+
+        # installing archiso if needed
+        package = "archiso"
+        fn.install_package(self, package)
+
+        # making sure we start with a clean slate
+        logging.info("Let's remove any old previous building folders")
+        fn.remove_dir(self, "/tmp/arcoinstall/")
+        fn.remove_dir(self, "/tmp/archlive/")
+
+        # git clone the iso scripts
+        command = (
+            "git clone https://github.com/arconetpro/arcoinstall.git" + " /tmp/arcoinstall"
+        )
+
+        logging.info("git cloning the build folder")
+        try:
+            fn.run_command(command)
+        except Exception as error:
+            logging.error(error)
+
+        # launch the scripts
+        logging.info("Start building the iso in Alacritty")
+        logging.info(
+            "#################################################################"
+        )
+        logging.info("Sometimes you have to try and build it a second time")
+        logging.info("for it to work because of keys, servers and network connections")
+        logging.info(
+            "##################################################################"
+        )
+        logging.info("Changed to /tmp/arcoinstall")
+        fn.os.chdir("/tmp/arcoinstall")
+
+        # Preparing to launch the build
+        command = "/tmp/arcoinstall/build_iso.sh"
+
+        logging.info("Launching the building script")
+
+        # Checking whether switch is on
+        critty = "alacritty -e "
+        logging.info(critty + command)
+        # Launching the build
+        try:
+            fn.subprocess.call(
+                critty + command,
+                shell=True,
+                stdout=fn.subprocess.PIPE,
+                stderr=fn.subprocess.STDOUT,
+            )
+        except Exception as error:
+            logging.error(error)
+
+        # change the output - foldername
+        dir = "arcoinstall-Out"
+
+        # Moving the iso to home directory of the user
+        path_dir = "/tmp/archlive/out/"
+        destination = fn.home + "/" + dir
+        logging.info("Move folder to home directory of the user")
+        try:
+            os.makedirs(os.path.dirname(destination), exist_ok=True)
+            fn.shutil.copytree(path_dir, destination, dirs_exist_ok=True)
+
+            # Sending an in-app message
+            GLib.idle_add(
+                fn.show_in_app_notification,
+                self,
+                "The creation of the Arcoinstall iso is finished",
+                False,
+            )
+        except Exception as error:
+            logging.error(error)
+
+        # changing permission
+        fn.permissions(destination)
+        logging.info("Check your home directory for the iso")
+
+
     def on_clean_pacman_cache_clicked(self, widget):
         # Cleaning the /var/cache/pacman/pkg/
         logging.info("Let's clean the pacman cache")
